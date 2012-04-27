bc45a753310a3024f00c8afa4a5f6a676e821e66
Garrett Schlesinger <gschles@cs.stanford.edu>
2012-04-27 11:23:18 -0700

    screwed up loading the PA2 code

--- cs276pa2/buildmodels.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-# ./buildmodels.sh <training corpus dir> <training edit1s file>
-
-python models.py $1 $2
--- cs276pa2/runcorrector.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-# ./corrector.sh <dev | test> <uniform | emperical> <queries file>
-
-python corrector.py $1 $2 $3
