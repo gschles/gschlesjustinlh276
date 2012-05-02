e1114dcf40752cec0ecafda097e1386620ba952d
Garret Schlesinger <gschles@cs.stanford.edu>
2012-05-01 20:03:35 -0700

    counts bigrams and unigrams

--- cs276pa2/models.py
+++ cs276pa2/models.py
@@ -3,11 +3,16 @@ import sys
 import os.path
 import gzip
 from glob import iglob
+from collections import Counter
 
 def scan_corpus(training_corpus_loc):
   """
   Scans through the training corpus and counts how many lines of text there are
+  Functionality added by Garrett: now counts all unigrams
   """
+  word_counts = Counter()
+  bigram_counts = Counter()
+
   for block_fname in iglob( os.path.join( training_corpus_loc, '*.gz' ) ):
     print >> sys.stderr, 'processing dir: ' + block_fname
     with gzip.open( block_fname ) as f:
@@ -15,8 +20,19 @@ def scan_corpus(training_corpus_loc):
       for line in f:
         # remember to remove the trailing \n
         line = line.rstrip()
+        words = line.split()
+        prev_word = None
+        for word in words:
+          word_counts[word] += 1
+          if prev_word is not None:
+            bigram_counts[(prev_word, word)] += 1
+          prev_word = word
         num_lines += 1
       print >> sys.stderr, 'Number of lines in ' + block_fname + ' is ' + str(num_lines)
+  print >> sys.stderr, 'Processed ' + str(sum(word_counts.itervalues())) + ' tokens'
+  print  >> sys.stderr, 'Processed ' + str(len(word_counts)) + ' unique types'
+  print >> sys.stderr, 'Processed ' + str(sum(bigram_counts.itervalues())) + ' bigrams'
+  print  >> sys.stderr, 'Processed ' + str(len(bigram_counts)) + ' unique bigrams'
 
 def read_edit1s():
   """
@@ -31,3 +47,4 @@ def read_edit1s():
 
 if __name__ == '__main__':
   print(sys.argv)
+  scan_corpus(sys.argv[1])
